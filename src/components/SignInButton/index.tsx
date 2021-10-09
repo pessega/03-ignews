98e3b98dadcce9b2d3361a8ef90fe82c67b4e433
pessega <andressaxduarte@gmail.com>
2021-10-09 08:39:57 -0300

    webhooks

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,14 +1,19 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <>
-      <button type="button" className={styles.siginInButtoon}>
+      <button
+        type="button"
+        className={styles.siginInButtoon}
+        onClick={() => signOut()}
+      >
         <FaGithub color="#04d361" />
         Andressa Duarte
         <FiX color="#737380" className={styles.closeIcon} />
@@ -16,7 +21,11 @@ export function SignInButton() {
     </>
   ) : (
     <>
-      <button type="button" className={styles.siginInButtoon}>
+      <button
+        type="button"
+        className={styles.siginInButtoon}
+        onClick={() => signIn("github")}
+      >
         <FaGithub color="#eba417" />
         SignIn with Github
       </button>
